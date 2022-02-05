6e240e15b985b233dac237eb7519417117bfdeec
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-02-05 08:50:16 -0300

    fix: build

--- src/ChromieXP.cpp
+++ src/ChromieXP.cpp
@@ -237,7 +237,7 @@ public:
             return true;
         }
 
-        auto result = CharacterDatabase.PQuery(SELECT_TESTER_QUERY, player->GetGUID().GetCounter());
+        auto result = CharacterDatabase.Query(SELECT_TESTER_QUERY, player->GetGUID().GetCounter());
 
         if (result && result->GetRowCount() > 0)
         {
@@ -245,7 +245,7 @@ public:
             return true;
         }
 
-        CharacterDatabase.PQuery(INSERT_TESTER_QUERY, player->GetGUID().GetCounter(), player->GetName());
+        CharacterDatabase.Query(INSERT_TESTER_QUERY, player->GetGUID().GetCounter(), player->GetName());
 
         if (player->HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_NO_XP_GAIN))
         {
